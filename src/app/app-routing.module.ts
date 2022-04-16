e452f46494ae9b519527afe85465aba3709efad0
katina-anachkova <katianachkova@gmail.com>
2022-04-16 08:33:41 +0300

    404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CreateBookComponent } from './feature/books/create-book/create-book.com
 import { DashboardComponent } from './feature/pages/dashboard/dashboard.component';
 import { EditPageComponent } from './feature/books/edit-page/edit-page.component';
 import { DetailsPageComponent } from './feature/books/details-page/details-page.component';
+import { PageNotFoundComponent } from './feature/pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -25,6 +26,10 @@ const routes: Routes = [
 {
   path: 'details/:id',
   component: DetailsPageComponent
+},
+{
+  path: '**',
+  component: PageNotFoundComponent
 }
 ];
 
